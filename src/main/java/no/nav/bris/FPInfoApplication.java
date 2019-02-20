d3a997f3ce12e1aabc5e044dc27c1aff3ddbea13
Jan-Olav Eide <janolaveide@gmail.com>
2019-02-20 12:21:12 +0100

    first shot

---
+++ src/main/java/no/nav/bris/FPInfoApplication.java
@@ -0,0 +1,15 @@
+package no.nav.bris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
+
+@EnableOIDCTokenValidation(ignore = { "org.springframework", "springfox.documentation" })
+@SpringBootApplication
+public class FPInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FPInfoApplication.class, args);
+    }
+}
---
+++ src/test/java/no/nav/bris/kafka/SpringKafkaApplicationTest.java
@@ -0,0 +1,39 @@
+package no.nav.bris.kafka;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import no.nav.bris.kafka.Receiver;
+import no.nav.bris.kafka.Sender;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, topics = { SpringKafkaApplicationTest.HELLOWORLD_TOPIC })
+public class SpringKafkaApplicationTest {
+
+    static final String HELLOWORLD_TOPIC = "helloworld.t";
+
+    @Autowired
+    private Receiver receiver;
+
+    @Autowired
+    private Sender sender;
+
+    @Test
+    public void testReceive() throws Exception {
+        sender.send("Hello Spring Kafka!");
+
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+    }
+}
