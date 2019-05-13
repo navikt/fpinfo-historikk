2f90c26b4031dfdc0551b3c074388a26384fc106
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-13 16:55:23 +0200

    try read secret

--- src/main/java/no/nav/foreldrepenger/historikk/oppslag/OppslagController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/oppslag/OppslagController.java
@@ -2,6 +2,10 @@ package no.nav.foreldrepenger.historikk.oppslag;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +25,12 @@ public class OppslagController {
 
     @GetMapping("/ping")
     @Unprotected
-    public String ping(@RequestParam(name = "navn", defaultValue = "jordboer") String navn) {
+    public String ping(@RequestParam(name = "navn", defaultValue = "jordboer") String navn) throws IOException {
         LOG.info("Jeg ble pinget");
+        if (Files.exists(Paths.get("/var/run/secrets/nais.io/vault/vault_token"))) {
+            byte[] encoded = Files.readAllBytes(Paths.get("/var/run/secrets/nais.io/vault/vault_token"));
+            return new String(encoded, "UTF-8").trim();
+        }
         return "Hallo " + navn + " fra ubeskyttet ressurs";
     }
 }
