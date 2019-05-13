c7a7cd147474a98ff14f96a4ecec243414bffb1f
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-13 09:16:33 +0200

    skip kafka for now

--- src/main/java/no/nav/foreldrepenger/historikk/kafka/MessageListener.java
+++ src/main/java/no/nav/foreldrepenger/historikk/kafka/MessageListener.java
@@ -2,17 +2,13 @@ package no.nav.foreldrepenger.historikk.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MessageListener {
-
     private static final Logger LOG = LoggerFactory.getLogger(MessageListener.class);
 
-    @KafkaListener(topics = "poc", groupId = "foo")
+    // @KafkaListener(topics = "poc", groupId = "foo")
     public void listen(String message) {
         LOG.info("Received message in group 'foo': {}", message);
     }
-
 }
