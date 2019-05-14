629b9b8b2506e2fd11209c575bccee50f90f1e0e
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-14 09:03:33 +0200

    vault token galore

--- src/main/java/no/nav/foreldrepenger/historikk/oppslag/OppslagController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/oppslag/OppslagController.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.historikk.oppslag;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bettercloud.vault.SslConfig;
+import com.bettercloud.vault.Vault;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.VaultException;
+import com.bettercloud.vault.response.LookupResponse;
 
 import no.nav.security.oidc.api.ProtectedWithClaims;
 import no.nav.security.oidc.api.Unprotected;
@@ -27,10 +34,42 @@ public class OppslagController {
     @Unprotected
     public String ping(@RequestParam(name = "navn", defaultValue = "jordboer") String navn) throws IOException {
         LOG.info("Jeg ble pinget");
-        if (Files.exists(Paths.get("/var/run/secrets/nais.io/vault/vault_token"))) {
-            byte[] encoded = Files.readAllBytes(Paths.get("/var/run/secrets/nais.io/vault/vault_token"));
-            return new String(encoded, "UTF-8").trim();
+        String token = getToken();
+        VaultConfig vaultConfig = null;
+        try {
+            vaultConfig = new VaultConfig()
+                    .address(System.getenv().getOrDefault("VAULT_ADDR", "https://vault.adeo.no"))
+                    .token(token)
+                    .openTimeout(5)
+                    .readTimeout(30)
+                    .sslConfig(new SslConfig().build())
+                    .build();
+        } catch (VaultException e) {
+            LOG.warn("Oops", e);
+            return "Could not instantiate the Vault REST client";
         }
-        return "Hallo " + navn + " fra ubeskyttet ressurs";
+        Vault vault = new Vault(vaultConfig);
+        try {
+            LookupResponse lookupSelf = vault.auth().lookupSelf();
+            if (lookupSelf.isRenewable()) {
+                return "token is renewable";
+            }
+            return "token is not renewable";
+        } catch (VaultException e) {
+            LOG.warn("Oops 1", e);
+            if (e.getHttpStatusCode() == 403) {
+                return "The application's vault token seems to be invalid";
+            } else {
+                return "Could not validate the application's vault token";
+            }
+        }
+    }
+
+    private String getToken() throws IOException {
+        if (Paths.get("/var/run/secrets/nais.io/vault/vault_token").toFile().exists()) {
+            byte[] encoded = Files.readAllBytes(Paths.get("/var/run/secrets/nais.io/vault/vault_token"));
+            return new String(encoded, StandardCharsets.UTF_8).trim();
+        }
+        return null;
     }
 }
