a7c9fec23ac3fb1fccb44864620f85986759cb4a
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-20 21:42:30 +0200

    try see it username is set on construction

--- src/main/java/no/nav/foreldrepenger/historikk/oppslag/VaultHikariConfig.java
+++ src/main/java/no/nav/foreldrepenger/historikk/oppslag/VaultHikariConfig.java
@@ -3,8 +3,10 @@ package no.nav.foreldrepenger.historikk.oppslag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.vault.config.databases.VaultDatabaseProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.vault.core.lease.LeaseEndpoints;
 import org.springframework.vault.core.lease.SecretLeaseContainer;
@@ -24,8 +26,8 @@ public class VaultHikariConfig implements InitializingBean {
     private String vaultPostgresRole;
     private final SecretLeaseContainer container;
     private final HikariDataSource hikariDataSource;
-    @Value("${spring.database.username}")
-    private String username;
+    @Autowired
+    private VaultDatabaseProperties properties;
 
     public VaultHikariConfig(SecretLeaseContainer container, HikariDataSource hikariDataSource) {
         this.container = container;
