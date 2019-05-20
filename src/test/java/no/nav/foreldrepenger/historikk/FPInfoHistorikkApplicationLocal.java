126182c12f6f982f9fa37287d7822ad66ab9c22b
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-20 11:19:00 +0200

    test jdbc

--- src/test/java/no/nav/foreldrepenger/historikk/FPInfoHistorikkApplicationLocal.java
+++ src/test/java/no/nav/foreldrepenger/historikk/FPInfoHistorikkApplicationLocal.java
@@ -5,14 +5,11 @@ import java.io.IOException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-import no.nav.foreldrepenger.historikk.oppslag.VaultUtils;
-
 @SpringBootApplication
 public class FPInfoHistorikkApplicationLocal {
     public static void main(String[] args) throws IOException {
         new SpringApplicationBuilder(FPInfoHistorikkApplicationLocal.class)
                 .profiles("dev", "preprod")
-                .properties("spring.cloud.vault.token=" + VaultUtils.getToken())
                 .main(FPInfoHistorikkApplicationLocal.class)
                 .run(args);
     }
