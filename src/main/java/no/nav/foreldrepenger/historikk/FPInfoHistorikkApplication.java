696b54606300b4c4f0fff9723e04a97dd80704b3
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-20 20:55:24 +0200

    ds

--- src/main/java/no/nav/foreldrepenger/historikk/FPInfoHistorikkApplication.java
+++ src/main/java/no/nav/foreldrepenger/historikk/FPInfoHistorikkApplication.java
@@ -1,18 +1,38 @@
 package no.nav.foreldrepenger.historikk;
 
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation;
 
 @EnableOIDCTokenValidation(ignore = { "org.springframework", "springfox.documentation" })
 @SpringBootApplication
-@SpringBootConfiguration
+@RestController
 public class FPInfoHistorikkApplication {
+    @Value("${vault.secret}")
+    private String vaultSecret;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(FPInfoHistorikkApplication.class)
                 .main(FPInfoHistorikkApplication.class)
                 .run(args);
+    }
+
+    @GetMapping("/ready")
+    public String ready() {
+        jdbcTemplate.execute("SELECT 1 ");
+        return "OK";
+    }
+
+    @GetMapping("/secret")
+    public String secret() {
+        return vaultSecret;
     }
 }
