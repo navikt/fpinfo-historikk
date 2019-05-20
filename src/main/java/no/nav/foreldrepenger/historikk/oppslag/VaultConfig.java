b5d8a7e061a185a952bd62301ce5a7317f534963
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-20 09:26:20 +0200

    try again

--- src/main/java/no/nav/foreldrepenger/historikk/oppslag/VaultConfig.java
+++ src/main/java/no/nav/foreldrepenger/historikk/oppslag/VaultConfig.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.vault.config.databases.VaultDatabaseProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.vault.core.lease.LeaseEndpoints;
 import org.springframework.vault.core.lease.SecretLeaseContainer;
 import org.springframework.vault.core.lease.domain.RequestedSecret;
 import org.springframework.vault.core.lease.event.SecretLeaseCreatedEvent;
@@ -16,21 +17,27 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 @ConditionalOnProperty(value = "vault.enabled", matchIfMissing = true)
 public class VaultConfig implements InitializingBean {
+    private static Logger LOGGER = LoggerFactory.getLogger(VaultConfig.class.getName());
+    @Value("${vault.postgres.backend}")
+    private String vaultPostgresBackend;
+    @Value("${vault.postgres.role}")
+    private String vaultPostgresRole;
     @Autowired
     private SecretLeaseContainer container;
     @Autowired
     private HikariDataSource hikariDataSource;
-    @Autowired
-    private VaultDatabaseProperties properties;
-    private static final Logger LOG = LoggerFactory.getLogger(VaultConfig.class);
 
     @Override
     public void afterPropertiesSet() {
-        RequestedSecret secret = RequestedSecret.rotating(properties.getBackend() + "/creds/" + properties.getRole());
+        // todo Remove when
+        // https://github.com/spring-cloud/spring-cloud-vault/issues/333 has been
+        // resolved
+        container.setLeaseEndpoints(LeaseEndpoints.SysLeases);
+        RequestedSecret secret = RequestedSecret
+                .rotating(this.vaultPostgresBackend + "/creds/" + this.vaultPostgresRole);
         container.addLeaseListener(leaseEvent -> {
-            LOG.info("Vault: Lease Event: {}", leaseEvent);
             if (leaseEvent.getSource() == secret && leaseEvent instanceof SecretLeaseCreatedEvent) {
-                LOG.info("Vault: Refreshing database credentials. Lease Event: {}", leaseEvent);
+                LOGGER.info("Rotating creds for path: " + leaseEvent.getSource().getPath());
                 SecretLeaseCreatedEvent slce = (SecretLeaseCreatedEvent) leaseEvent;
                 String username = slce.getSecrets().get("username").toString();
                 String password = slce.getSecrets().get("password").toString();
