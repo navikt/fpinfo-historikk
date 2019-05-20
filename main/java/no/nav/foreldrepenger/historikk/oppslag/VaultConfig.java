a1f92be42ca37c7e1354239221cb168e77e4f403
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-20 11:29:48 +0200

    clean

--- main/java/no/nav/foreldrepenger/historikk/oppslag/VaultConfig.java
+++ main/java/no/nav/foreldrepenger/historikk/oppslag/VaultConfig.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.vault.core.lease.LeaseEndpoints;
 import org.springframework.vault.core.lease.SecretLeaseContainer;
 import org.springframework.vault.core.lease.domain.RequestedSecret;
@@ -14,8 +12,8 @@ import org.springframework.vault.core.lease.event.SecretLeaseCreatedEvent;
 
 import com.zaxxer.hikari.HikariDataSource;
 
-@Configuration
-@ConditionalOnProperty(value = "vault.enabled", matchIfMissing = true)
+//@Configuration
+//@ConditionalOnProperty(value = "vault.enabled", matchIfMissing = true)
 public class VaultConfig implements InitializingBean {
     private static Logger LOGGER = LoggerFactory.getLogger(VaultConfig.class.getName());
     @Value("${spring.cloud.vault.database.backend}")
