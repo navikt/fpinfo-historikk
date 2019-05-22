9067248ecb9118de19f50583e1e5bdb881d6d537
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-22 15:35:19 +0200

    topic name now a property

--- src/main/java/no/nav/foreldrepenger/historikk/kafka/Producer.java
+++ src/main/java/no/nav/foreldrepenger/historikk/kafka/Producer.java
@@ -1,21 +1,34 @@
 package no.nav.foreldrepenger.historikk.kafka;
 
+import static no.nav.foreldrepenger.historikk.util.EnvUtil.DEV;
+import static no.nav.foreldrepenger.historikk.util.EnvUtil.PREPROD;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-//@Profile(EnvUtil.DEV)
+@Profile({ DEV, PREPROD })
 public class Producer {
-    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
-    private static final String TOPIC = "fpinfo-historikk";
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private static final Logger LOG = LoggerFactory.getLogger(Producer.class);
+    private final String topic;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public Producer(@Value("${kafka.topic}") String topic, KafkaTemplate<String, String> kafkaTemplate) {
+        this.topic = topic;
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     public void sendMessage(String message) {
-        logger.info(String.format("#### -> Producing message -> %s", message));
-        this.kafkaTemplate.send(TOPIC, message);
+        LOG.info(String.format("#### -> Producing message -> %s", message));
+        this.kafkaTemplate.send(topic, message);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [topic=" + topic + ", kafkaTemplate=" + kafkaTemplate + "]";
     }
 }
