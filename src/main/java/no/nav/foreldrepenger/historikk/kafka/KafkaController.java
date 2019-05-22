938bb04f2b113c2b51194919c3b9b68c34de59f5
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-22 12:21:01 +0200

    log more

--- src/main/java/no/nav/foreldrepenger/historikk/kafka/KafkaController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/kafka/KafkaController.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import no.nav.security.oidc.api.Unprotected;
 
-@RestController
+//@RestController
 //@Profile(EnvUtil.DEV)
 @RequestMapping(value = "/kafka")
 public class KafkaController {
