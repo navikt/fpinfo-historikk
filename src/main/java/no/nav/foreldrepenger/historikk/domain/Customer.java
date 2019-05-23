b655c883d8dee6b7f5d3c4065c234bf844a874df
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-23 14:28:58 +0200

    ID

--- src/main/java/no/nav/foreldrepenger/historikk/domain/Customer.java
+++ src/main/java/no/nav/foreldrepenger/historikk/domain/Customer.java
@@ -2,12 +2,13 @@ package no.nav.foreldrepenger.historikk.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Customer {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     public void setId(int id) {
