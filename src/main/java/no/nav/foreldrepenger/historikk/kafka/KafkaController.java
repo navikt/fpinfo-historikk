c7e6e480a172508dc3da73900d91593cbe441d98
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-24 14:14:57 +0200

    serialize and save proper object

--- src/main/java/no/nav/foreldrepenger/historikk/kafka/KafkaController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/kafka/KafkaController.java
@@ -3,33 +3,27 @@ package no.nav.foreldrepenger.historikk.kafka;
 import static no.nav.foreldrepenger.historikk.util.EnvUtil.DEV;
 import static no.nav.foreldrepenger.historikk.util.EnvUtil.PREPROD;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import no.nav.foreldrepenger.historikk.domain.Customer;
-import no.nav.foreldrepenger.historikk.repository.CustomerRepository;
+import no.nav.foreldrepenger.historikk.domain.Melding;
+import no.nav.foreldrepenger.historikk.repository.MeldingRepository;
 import no.nav.security.oidc.api.Unprotected;
 
 @RestController
 @Profile({ DEV, PREPROD })
 @RequestMapping(value = "/kafka")
+@Unprotected
 public class KafkaController {
     private final Producer producer;
-    private static final Logger LOG = LoggerFactory.getLogger(KafkaController.class);
     @Autowired
-    private CustomerRepository repository;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private MeldingRepository repository;
 
     @Autowired
     KafkaController(Producer producer) {
@@ -37,22 +31,12 @@ public class KafkaController {
     }
 
     @GetMapping(value = "/publish")
-    @Unprotected
-    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
-        this.producer.sendMessage(message);
+    public void sendMessageToKafkaTopic(@RequestParam("melding") String melding, @RequestParam("fnr") String fnr) {
+        this.producer.sendMelding(new Melding(fnr, melding));
     }
 
-    @GetMapping("/count")
-    @Unprotected
-    public List<String> count() {
-        jdbcTemplate.queryForObject("SELECT COUNT(FIRST_NAME) FROM CUSTOMER", Integer.class);
-        LOG.info("Customers found with findAll():");
-        List<String> list = new ArrayList<>();
-        LOG.info("-------------------------------");
-        for (Customer customer : repository.findAll()) {
-            LOG.info("customer " + customer.toString());
-            list.add(customer.getId() + "," + customer.getFirstName() + "," + customer.getLastName());
-        }
-        return list;
+    @GetMapping("/find")
+    public List<Melding> findByFnr(@RequestParam("fnr") String fnr) {
+        return repository.findByFnr(fnr);
     }
 }
