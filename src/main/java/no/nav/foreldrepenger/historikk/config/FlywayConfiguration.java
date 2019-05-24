132e08184ab19ea1cf0a003d6d7aa8ab573d64c0
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-24 11:17:30 +0200

    max pool size is 2

--- src/main/java/no/nav/foreldrepenger/historikk/config/FlywayConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/FlywayConfiguration.java
@@ -1,13 +1,17 @@
 package no.nav.foreldrepenger.historikk.config;
 
+import static no.nav.foreldrepenger.historikk.util.EnvUtil.PREPROD;
+
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile({ PREPROD })
 public class FlywayConfiguration {
     @Bean
     public FlywayConfigurationCustomizer flywayConfig() {
