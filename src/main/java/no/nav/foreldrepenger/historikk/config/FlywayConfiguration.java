16386e7c529c468bd20117c7d4bbfdb72b89820e
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-05-30 17:10:55 +0200

    role from property

--- src/main/java/no/nav/foreldrepenger/historikk/config/FlywayConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/FlywayConfiguration.java
@@ -2,6 +2,9 @@ package no.nav.foreldrepenger.historikk.config;
 
 import static no.nav.foreldrepenger.historikk.util.EnvUtil.PREPROD;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +12,13 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class FlywayConfiguration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FlywayConfiguration.class);
+
     @Bean
     @Profile({ PREPROD })
-    public FlywayConfigurationCustomizer flywayConfig() {
+    public FlywayConfigurationCustomizer flywayConfig(@Value("${spring.cloud.vault.database.role}") String role) {
+        LOG.info("Fikk rolle " + role);
         return c -> c.initSql("SET ROLE \"fpinfo-historikk-preprod-admin\"");
     }
 }
