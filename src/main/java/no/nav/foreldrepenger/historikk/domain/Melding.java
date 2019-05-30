ff2f6217accb2c292d58081614214315e81c8f81
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-05-30 13:03:46 +0200

    add date

--- src/main/java/no/nav/foreldrepenger/historikk/domain/Melding.java
+++ src/main/java/no/nav/foreldrepenger/historikk/domain/Melding.java
@@ -1,13 +1,24 @@
 package no.nav.foreldrepenger.historikk.domain;
 
+import java.time.LocalDate;
+
 public class Melding {
 
     private final AktørId aktørId;
     private final String melding;
+    private LocalDate dato;
 
     public Melding(AktørId aktørId, String melding) {
         this.aktørId = aktørId;
         this.melding = melding;
+    }
+
+    public LocalDate getDato() {
+        return dato;
+    }
+
+    public void setDato(LocalDate dato) {
+        this.dato = dato;
     }
 
     public AktørId getAktørId() {
