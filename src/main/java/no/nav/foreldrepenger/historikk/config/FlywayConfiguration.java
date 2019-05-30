9a663e742718444709ac996264039d60aa7d0a57
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-05-30 17:26:49 +0200

    initsql only in cluster

--- src/main/java/no/nav/foreldrepenger/historikk/config/FlywayConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/FlywayConfiguration.java
@@ -1,24 +1,19 @@
 package no.nav.foreldrepenger.historikk.config;
 
-import static no.nav.foreldrepenger.historikk.util.EnvUtil.PREPROD;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
+@ConditionalOnProperty("spring.flyway.enabled")
 public class FlywayConfiguration {
 
-    private static final Logger LOG = LoggerFactory.getLogger(FlywayConfiguration.class);
-
     @Bean
-    @Profile({ PREPROD })
+    @Profile("!dev")
     public FlywayConfigurationCustomizer flywayConfig(@Value("${spring.cloud.vault.database.role}") String role) {
-        LOG.info("Fikk rolle " + role);
-        return c -> c.initSql("SET ROLE \"fpinfo-historikk-preprod-admin\"");
+        return c -> c.initSql(String.format("SET ROLE \"%s\"", role));
     }
 }
