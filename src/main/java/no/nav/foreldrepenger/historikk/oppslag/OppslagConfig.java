088c58188154248f53d017a0416cad80948f7bcc
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-05-31 12:46:22 +0200

    hardcode

--- src/main/java/no/nav/foreldrepenger/historikk/oppslag/OppslagConfig.java
+++ src/main/java/no/nav/foreldrepenger/historikk/oppslag/OppslagConfig.java
@@ -1,7 +1,6 @@
 package no.nav.foreldrepenger.historikk.oppslag;
 
 import java.net.URI;
-import java.util.Optional;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -12,25 +11,14 @@ public class OppslagConfig {
     private static final String AKTØR = "oppslag/aktor";
     private static final URI DEFAULT_BASE_URI = URI.create("http://fpsoknad-oppslag/api");
     private static final String DEFAULT_PING_PATH = "actuator/info";
-    private String pingPath;
-    private String aktørPath;
     private boolean enabled;
-    private URI baseURI;
 
     public String getAktørPath() {
-        return Optional.ofNullable(aktørPath).orElse(AKTØR);
-    }
-
-    public void setAktørPath(String aktørPath) {
-        this.aktørPath = aktørPath;
+        return AKTØR;
     }
 
     public URI getBaseURI() {
-        return Optional.ofNullable(baseURI).orElse(DEFAULT_BASE_URI);
-    }
-
-    public void setBaseURI(URI baseURI) {
-        this.baseURI = baseURI;
+        return DEFAULT_BASE_URI;
     }
 
     public boolean isEnabled() {
@@ -42,16 +30,7 @@ public class OppslagConfig {
     }
 
     public String getPingPath() {
-        return Optional.ofNullable(pingPath).orElse(DEFAULT_PING_PATH);
+        return DEFAULT_PING_PATH;
     }
 
-    public void setPingPath(String pingPath) {
-        this.pingPath = pingPath;
-    }
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + " [pingPath=" + pingPath + ", enabled=" + enabled + ", url=" + baseURI
-                + "]";
-    }
 }
