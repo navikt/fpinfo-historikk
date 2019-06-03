0cbd41fbea210e670de576cdb920ff51e88bbe6d
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-03 09:15:24 +0200

    lagt til leveransekanal

---
+++ src/main/java/no/nav/foreldrepenger/historikk/domain/LeveranseKanal.java
@@ -0,0 +1,6 @@
+package no.nav.foreldrepenger.historikk.domain;
+
+public enum LeveranseKanal {
+    RESERVERT, ELEKRONISK_POSTKASSE, DIREKTE
+
+}
--- src/main/resources/db/migration/V1__1.sql
+++ src/main/resources/db/migration/V1__1.sql
@@ -3,5 +3,6 @@ CREATE TABLE MELDING (
     aktør_id VARCHAR (50)  NOT NULL,
     melding VARCHAR (2000)  NOT NULL,
     dato DATE NOT NULL DEFAULT CURRENT_DATE,
-    saksnr VARCHAR(50) NOT NULL
+    saksnr VARCHAR(50) NOT NULL,
+    kanal VARCHAR(50) NOT NULL
 );
