109dcb0a787738d1d5bbbddfe276f43175bb8704
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-03 16:07:46 +0200

    add columnt lest, only return rows where it is null

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
@@ -3,9 +3,11 @@ package no.nav.foreldrepenger.historikk.meldinger;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import no.nav.foreldrepenger.historikk.meldinger.dto.JPAMelding;
 
 public interface MeldingsLagerRepository extends JpaRepository<JPAMelding, Long> {
+    @Query("SELECT m FROM Melding m WHERE m.lest IS NULL")
     List<JPAMelding> findByAktørId(String aktørId);
 }
