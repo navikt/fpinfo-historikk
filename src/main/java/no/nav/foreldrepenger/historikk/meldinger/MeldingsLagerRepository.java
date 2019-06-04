576a46a3bee5aae7778aa412dc55dee4c3d422d8
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-04 09:04:08 +0200

    forgot placeholder

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import no.nav.foreldrepenger.historikk.meldinger.dto.JPAMelding;
 
 public interface MeldingsLagerRepository extends JpaRepository<JPAMelding, Long> {
-    @Query("SELECT m FROM MELDING m WHERE m.lest IS NULL")
+    @Query("SELECT m FROM MELDING m WHERE m.lest IS NULL AND m.aktør_id = ?1")
     List<JPAMelding> findByAktørId(String aktørId);
 }
