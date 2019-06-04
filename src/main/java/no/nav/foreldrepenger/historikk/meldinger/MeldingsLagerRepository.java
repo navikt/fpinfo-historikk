f7e798d1325d063d261f4f10be6f7b36c29b0028
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-04 10:06:29 +0200

    jpql fixed, only fetch unread

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
@@ -1,8 +1,10 @@
 package no.nav.foreldrepenger.historikk.meldinger;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import no.nav.foreldrepenger.historikk.meldinger.dto.JPAMelding;
@@ -10,4 +12,8 @@ import no.nav.foreldrepenger.historikk.meldinger.dto.JPAMelding;
 public interface MeldingsLagerRepository extends JpaRepository<JPAMelding, Long> {
     @Query("SELECT m FROM JPAMelding m WHERE m.aktørId = ?1 AND m.lest IS NULL")
     List<JPAMelding> finnUlesteMeldinger(String aktørId);
+
+    @Modifying
+    @Query("UPDATE JPAMelding m SET m.lest = ?2 WHERE m.aktørId = ?1")
+    void merkAlle(String aktørId, LocalDate date);
 }
