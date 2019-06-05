f280e8dd5c5ab8a36ab41f9b4a8b86a589408412
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-05 12:43:49 +0200

    transavctional

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import no.nav.foreldrepenger.historikk.meldinger.dto.JPAMelding;
 
+@Transactional
 public interface MeldingsLagerRepository extends JpaRepository<JPAMelding, Long> {
     @Query("SELECT m FROM JPAMelding m WHERE m.aktørId = ?1 AND m.lest IS NULL")
     List<JPAMelding> finnUlesteMeldinger(String aktørId);
