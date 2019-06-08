3f3a7e7a861c0f19575b6c838cfe389f1cd428fc
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-08 11:38:33 +0200

    revert

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingKonsument.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingKonsument.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
 import no.nav.foreldrepenger.historikk.domain.Melding;
@@ -27,9 +26,9 @@ public class MeldingKonsument {
     }
 
     @KafkaListener(topics = "#{'${no.nav.foreldrepenger.historikk.kafka.meldinger.topic}'}", groupId = "#{'${spring.kafka.consumer.group-id}'}")
-    public void listen(String json, Acknowledgment ack, @Header("X-Custom-Header") String customHeader) {
+    public void listen(String json, Acknowledgment ack) {
         Melding melding = mapper.convert(json, Melding.class);
-        LOG.info("Mottok melding {} med header {}", json, customHeader);
+        LOG.info("Mottok melding {}", json);
         meldingsLager.lagre(melding);
         ack.acknowledge();
     }
