c9c5ce381e328a936c1af63c1d26aa0708a18410
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-08 11:29:16 +0200

    revert

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingProdusent.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingProdusent.java
@@ -8,8 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 import no.nav.foreldrepenger.historikk.domain.Melding;
@@ -19,21 +17,18 @@ import no.nav.foreldrepenger.historikk.domain.Melding;
 public class MeldingProdusent {
     private static final Logger LOG = LoggerFactory.getLogger(MeldingProdusent.class);
     private final String topic;
-    private final KafkaTemplate<String, Message<Melding>> kafkaTemplate;
+    private final KafkaTemplate<String, Melding> kafkaTemplate;
 
     public MeldingProdusent(@Value("${no.nav.foreldrepenger.historikk.kafka.meldinger.topic}") String topic,
-            KafkaTemplate<String, Message<Melding>> kafkaTemplate) {
+            KafkaTemplate<String, Melding> kafkaTemplate) {
         this.topic = topic;
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendMelding(Melding melding) {
-        Message<Melding> message = MessageBuilder
-                .withPayload(melding)
-                .setHeader("X-Custom-Header", "Sending Custom Header with Spring Kafka")
-                .build();
+
         LOG.info(String.format("Sender melding %s", melding));
-        this.kafkaTemplate.send(topic, message);
+        this.kafkaTemplate.send(topic, melding);
     }
 
     @Override
