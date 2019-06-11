533378c480fc3adeda72770e30dbb62fbb923658
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-11 12:49:00 +0200

    ignore for now

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/SøknadMottattKonsument.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/SøknadMottattKonsument.java
@@ -2,14 +2,13 @@ package no.nav.foreldrepenger.historikk.meldinger;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class SøknadMottattKonsument {
     private static final Logger LOG = LoggerFactory.getLogger(SøknadMottattKonsument.class);
 
-    @KafkaListener(topics = "#{'${historikk.kafka.meldinger.søknad_topic}'}", groupId = "#{'${spring.kafka.consumer.group-id}'}")
+    // @KafkaListener(topics = "#{'${historikk.kafka.meldinger.søknad_topic}'}",
+    // groupId = "#{'${spring.kafka.consumer.group-id}'}")
     public void listen(String json) {
         LOG.info("Mottok melding om søknad {}", json);
     }
