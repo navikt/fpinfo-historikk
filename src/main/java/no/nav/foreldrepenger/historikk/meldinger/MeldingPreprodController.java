5f70969eee0bb65efc54e42d3439df27fe2fe17d
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-12 10:47:33 +0200

    strings olnly

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingPreprodController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingPreprodController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import io.swagger.annotations.Api;
 import no.nav.foreldrepenger.historikk.domain.AktørId;
 import no.nav.foreldrepenger.historikk.domain.Melding;
@@ -35,7 +37,7 @@ public class MeldingPreprodController {
     }
 
     @PostMapping(value = "/publish")
-    public void sendMelding(@Valid @RequestBody Melding melding) {
+    public void sendMelding(@Valid @RequestBody Melding melding) throws JsonProcessingException {
         produsent.sendMelding(melding);
     }
 
