9df3f317a350d0565f8d06987657ab25fb750e2c
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-12 10:44:12 +0200

    strings olnly

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingProdusent.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingProdusent.java
@@ -3,6 +3,8 @@ package no.nav.foreldrepenger.historikk.meldinger;
 import static no.nav.foreldrepenger.historikk.util.EnvUtil.DEV;
 import static no.nav.foreldrepenger.historikk.util.EnvUtil.PREPROD;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +15,9 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import no.nav.foreldrepenger.historikk.config.Constants;
 import no.nav.foreldrepenger.historikk.domain.Melding;
@@ -25,20 +30,23 @@ public class MeldingProdusent {
     private static final Logger LOG = LoggerFactory.getLogger(MeldingProdusent.class);
     private final String topic;
     private final String søknad_topic;
-    private final KafkaTemplate<String, Melding> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     public MeldingProdusent(@Value("${historikk.kafka.meldinger.topic}") String topic,
             @Value("${historikk.kafka.meldinger.søknad_topic}") String søknad_topic,
-            KafkaTemplate<String, Melding> kafkaTemplate) {
+            KafkaTemplate<String, String> kafkaTemplate) {
         this.topic = topic;
         this.søknad_topic = søknad_topic;
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    @Inject
+    private ObjectMapper mapper;
+
     @Transactional("kafka")
-    public void sendMelding(Melding melding) {
-        Message<Melding> message = MessageBuilder
-                .withPayload(melding)
+    public void sendMelding(Melding melding) throws JsonProcessingException {
+        Message<String> message = MessageBuilder
+                .withPayload(mapper.writeValueAsString(melding))
                 .setHeader(KafkaHeaders.TOPIC, topic)
                 .setHeader(Constants.NAV_CALL_ID, MDCUtil.callIdOrNew())
                 .build();
