33b4ed6d34811a2ba820159dc5d038ecbaa86a05
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-17 11:09:07 +0200

    lest gone

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MeldingsLagerRepository.java
@@ -5,18 +5,12 @@ import static no.nav.foreldrepenger.historikk.config.TxConfiguration.JPA;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import no.nav.foreldrepenger.historikk.meldinger.dto.JPAMinidialogInnslag;
 
 @Transactional(JPA)
 public interface MeldingsLagerRepository extends JpaRepository<JPAMinidialogInnslag, Long> {
-
-    @Modifying
-    @Query("update JPAMinidialogInnslag m set m.lest = current_date where m.id = ?1")
-    void markerLest(long id);
 
     List<JPAMinidialogInnslag> findByAktørId(String aktørId);
 }
