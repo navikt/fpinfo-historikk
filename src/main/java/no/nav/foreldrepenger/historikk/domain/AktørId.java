2f149a64ba501d0bf7bcb5224ce95f6d80d390a5
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-18 13:57:19 +0200

    produser hendelse i prprod

--- src/main/java/no/nav/foreldrepenger/historikk/domain/AktørId.java
+++ src/main/java/no/nav/foreldrepenger/historikk/domain/AktørId.java
@@ -1,30 +1,24 @@
 package no.nav.foreldrepenger.historikk.domain;
 
-import javax.persistence.Embeddable;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-@Embeddable
 public class AktørId {
 
     @JsonValue
     private String aktørId;
+
+    @JsonCreator
+    public AktørId(@JsonProperty("aktørId") String aktørId) {
+        this.aktørId = aktørId;
+    }
 
     public String getAktørId() {
         return aktørId;
     }
 
     public void setAktørId(String aktørId) {
-        this.aktørId = aktørId;
-    }
-
-    private AktørId() {
-    }
-
-    @JsonCreator
-    public AktørId(@JsonProperty("aktørId") String aktørId) {
         this.aktørId = aktørId;
     }
 
