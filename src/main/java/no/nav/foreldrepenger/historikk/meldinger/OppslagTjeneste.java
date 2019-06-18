a1cf07d45a087aeeb4c74458811adc7609cdda3b
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-18 10:59:00 +0200

    disable mini dialog on event

---
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/OppslagTjeneste.java
@@ -0,0 +1,25 @@
+package no.nav.foreldrepenger.historikk.meldinger;
+
+import org.springframework.stereotype.Service;
+
+import no.nav.foreldrepenger.historikk.domain.AktørId;
+
+@Service
+public class OppslagTjeneste {
+
+    private final OppslagConnection connection;
+
+    public OppslagTjeneste(OppslagConnection connection) {
+        this.connection = connection;
+    }
+
+    public AktørId hentAktørId() {
+        return connection.hentAktørId();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[connection=" + connection + "]";
+    }
+
+}
