04ac8e34a4df6b5e8a162edc605009a1639dc51e
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-18 15:29:11 +0200

    also use gyldig_til

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MinidialogRepository.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MinidialogRepository.java
@@ -17,7 +17,8 @@ public interface MinidialogRepository extends JpaRepository<JPAMinidialogInnslag
 
     List<JPAMinidialogInnslag> findByAktørId(String aktørId);
 
-    List<JPAMinidialogInnslag> findByAktørIdAndAktivIsTrue(String aktørId);
+    @Query("Select m from JPAMinidialogInnslag m where m.gyldigTil <= CURRENT_DATE and m.aktørId = :aktørId and m.aktiv = true")
+    List<JPAMinidialogInnslag> findByAktørIdAndAktivIsTrue(@Param("aktørId") String aktørId);
 
     @Modifying
     @Query("update JPAMinidialogInnslag m set m.aktiv = false where m.aktørId = :aktørId and m.handling = :handling")
