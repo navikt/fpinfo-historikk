3d589cf1093e4f3c9e7b6353c7df9cbd08f5e89a
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-20 12:22:29 +0200

    minidialog-controller

---
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MinidialogController.java
@@ -0,0 +1,32 @@
+package no.nav.foreldrepenger.historikk.meldinger;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import no.nav.foreldrepenger.historikk.domain.MinidialogInnslag;
+import no.nav.security.oidc.api.ProtectedWithClaims;
+
+@RestController
+@RequestMapping(value = "/minidialog")
+@ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
+public class MinidialogController {
+
+    private final MinidialogTjeneste minidialog;
+
+    MinidialogController(MinidialogTjeneste minidialog) {
+        this.minidialog = minidialog;
+    }
+
+    @GetMapping("/me")
+    public List<MinidialogInnslag> hentMinidialog() {
+        return minidialog.hentMineAktiveDialoger();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [minidialog=" + minidialog + "]";
+    }
+}
