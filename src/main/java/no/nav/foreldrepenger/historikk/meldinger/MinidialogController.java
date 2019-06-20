19f019ff34f49ef4408e38cb9afd1631ad81cbc8
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-20 20:20:39 +0200

    cleanup

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/MinidialogController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/MinidialogController.java
@@ -2,8 +2,6 @@ package no.nav.foreldrepenger.historikk.meldinger;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +14,6 @@ import no.nav.security.oidc.api.ProtectedWithClaims;
 @ProtectedWithClaims(issuer = "selvbetjening", claimMap = { "acr=Level4" })
 public class MinidialogController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MinidialogController.class);
-
     private final MinidialogTjeneste minidialog;
 
     MinidialogController(MinidialogTjeneste minidialog) {
@@ -26,9 +22,7 @@ public class MinidialogController {
 
     @GetMapping("/me")
     public List<MinidialogInnslag> hentMinidialog() {
-        List<MinidialogInnslag> dialoger = minidialog.hentMineAktiveDialoger();
-        LOG.info("Hentet dialoger {}", dialoger);
-        return dialoger;
+        return minidialog.hentMineAktiveDialoger();
     }
 
     @Override
