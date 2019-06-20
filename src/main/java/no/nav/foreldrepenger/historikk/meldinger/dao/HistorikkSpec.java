704fdb51daa18d0e144738c92b6ab4baba0d9416
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-20 19:27:46 +0200

    typesafe Specifications using hibernate metadata

--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/dao/HistorikkSpec.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/dao/HistorikkSpec.java
@@ -1,5 +1,8 @@
 package no.nav.foreldrepenger.historikk.meldinger.dao;
 
+import static no.nav.foreldrepenger.historikk.meldinger.dao.JPAHistorikkInnslag_.aktørId;
+import static no.nav.foreldrepenger.historikk.meldinger.dao.JPAHistorikkInnslag_.opprettet;
+
 import java.time.LocalDate;
 
 import org.springframework.data.jpa.domain.Specification;
@@ -13,19 +16,11 @@ public class HistorikkSpec {
     }
 
     public static Specification<JPAHistorikkInnslag> harAktør(AktørId aktør) {
-        return (innslag, cq, cb) -> cb.equal(innslag.get("aktørId"), aktør.getAktørId());
-    }
-
-    public static Specification<JPAHistorikkInnslag> erAktiv() {
-        return (innslag, cq, cb) -> cb.equal(innslag.get("aktiv"), true);
+        return (innslag, cq, cb) -> cb.equal(innslag.get(aktørId), aktør.getAktørId());
     }
 
     public static Specification<JPAHistorikkInnslag> erEtter(LocalDate dato) {
-        return (innslag, cq, cb) -> cb.greaterThan(innslag.get("datoMottatt"), dato.atStartOfDay());
-    }
-
-    public static Specification<JPAHistorikkInnslag> erFør(LocalDate dato) {
-        return (innslag, cq, cb) -> cb.lessThan(innslag.get("datoMottatt"), dato.atStartOfDay());
+        return (innslag, cq, cb) -> cb.greaterThan(innslag.get(opprettet), dato.atStartOfDay());
     }
 
 }
--- src/main/java/no/nav/foreldrepenger/historikk/meldinger/dao/MinidialogSpec.java
+++ src/main/java/no/nav/foreldrepenger/historikk/meldinger/dao/MinidialogSpec.java
@@ -1,5 +1,10 @@
 package no.nav.foreldrepenger.historikk.meldinger.dao;
 
+import static no.nav.foreldrepenger.historikk.meldinger.dao.JPAMinidialogInnslag_.aktiv;
+import static no.nav.foreldrepenger.historikk.meldinger.dao.JPAMinidialogInnslag_.aktørId;
+import static no.nav.foreldrepenger.historikk.meldinger.dao.JPAMinidialogInnslag_.gyldigTil;
+import static no.nav.foreldrepenger.historikk.meldinger.dao.JPAMinidialogInnslag_.handling;
+
 import java.time.LocalDate;
 
 import org.springframework.data.jpa.domain.Specification;
@@ -14,19 +19,20 @@ public class MinidialogSpec {
     }
 
     public static Specification<JPAMinidialogInnslag> erGyldig() {
-        return (innslag, cq, cb) -> cb.greaterThanOrEqualTo(innslag.get("gyldigTil"), LocalDate.now());
+        return (innslag, cq, cb) -> cb.greaterThanOrEqualTo(innslag.get(gyldigTil),
+                LocalDate.now());
     }
 
     public static Specification<JPAMinidialogInnslag> erAktiv() {
-        return (innslag, cq, cb) -> cb.equal(innslag.get("aktiv"), true);
+        return (innslag, cq, cb) -> cb.equal(innslag.get(aktiv), true);
     }
 
     public static Specification<JPAMinidialogInnslag> harAktør(AktørId aktør) {
-        return (innslag, cq, cb) -> cb.equal(innslag.get("aktørId"), aktør.getAktørId());
+        return (innslag, cq, cb) -> cb.equal(innslag.get(aktørId), aktør.getAktørId());
     }
 
     public static Specification<JPAMinidialogInnslag> harHandling(SøknadType type) {
-        return (innslag, cq, cb) -> cb.equal(innslag.get("handling"), type.name());
+        return (innslag, cq, cb) -> cb.equal(innslag.get(handling), type.name());
     }
 
 }
