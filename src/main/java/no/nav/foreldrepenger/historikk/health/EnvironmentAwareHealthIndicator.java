ef925d481ef22293296d207d69dceed29f12c06e
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-21 17:18:58 +0200

    added health indicator for oppslag

---
+++ src/main/java/no/nav/foreldrepenger/historikk/health/EnvironmentAwareHealthIndicator.java
@@ -0,0 +1,55 @@
+package no.nav.foreldrepenger.historikk.health;
+
+import static no.nav.foreldrepenger.historikk.util.EnvUtil.isDevOrPreprod;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+import no.nav.foreldrepenger.historikk.http.PingEndpointAware;
+
+abstract class EnvironmentAwareHealthIndicator implements HealthIndicator, EnvironmentAware {
+    private final PingEndpointAware pingable;
+    private Environment env;
+
+    public EnvironmentAwareHealthIndicator(PingEndpointAware pingable) {
+        this.pingable = pingable;
+    }
+
+    @Override
+    public void setEnvironment(Environment env) {
+        this.env = env;
+    }
+
+    @Override
+    public Health health() {
+        try {
+            pingable.ping();
+            return up();
+        } catch (Exception e) {
+            return down(e);
+        }
+    }
+
+    private Health up() {
+        return isDevOrPreprod(env) ? Health.up()
+                .withDetail(pingable.name(), pingable.pingEndpoint())
+                .build()
+                : Health.up().build();
+    }
+
+    private Health down(Exception e) {
+        return isDevOrPreprod(env)
+                ? Health.down()
+                        .withDetail(pingable.name(), pingable.pingEndpoint())
+                        .withException(e)
+                        .build()
+                : Health.down().build();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [pingable=" + pingable + "isDevOrPreprod " + isDevOrPreprod(env) + "]";
+    }
+}
