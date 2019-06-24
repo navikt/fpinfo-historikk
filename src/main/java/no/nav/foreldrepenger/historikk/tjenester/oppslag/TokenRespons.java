5c7dd7296ee2a84c0e3550a95c0b93d3b8e97299
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-24 19:53:03 +0200

    constant, class for respons

---
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/oppslag/TokenRespons.java
@@ -0,0 +1,38 @@
+package no.nav.foreldrepenger.historikk.tjenester.oppslag;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TokenRespons {
+
+    private final String token;
+    private final String tokenType;
+    private final long expiresIn;
+
+    @JsonCreator
+    public TokenRespons(@JsonProperty("access_token") String access_token,
+            @JsonProperty("token_type") String token_type, @JsonProperty("expires_in") long expires_in) {
+        this.token = access_token;
+        this.tokenType = token_type;
+        this.expiresIn = expires_in;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[token=" + token + ", tokenType=" + tokenType + ", expiresIn="
+                + expiresIn + "]";
+    }
+
+}
