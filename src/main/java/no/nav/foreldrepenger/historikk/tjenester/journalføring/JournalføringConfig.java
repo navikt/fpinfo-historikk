704fe33be3db07035749692e10b02363b1079e42
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-24 14:45:04 +0200

    use q1 in preprod

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/journalføring/JournalføringConfig.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/journalføring/JournalføringConfig.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.historikk.tjenester.journalføring;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -14,8 +15,9 @@ public class JournalføringConfig {
     private static final String SERVICE = "http://dokarkiv";
     private static final String BASE_PATH = "/rest/journalpostapi/v1/";
     private static final URI DEFAULT_BASE_URI = URI.create(SERVICE + BASE_PATH);
-    private static final String DEFAULT_PING_PATH = "/actuator";
+    private static final String DEFAULT_PING_PATH = "actuator";
     private boolean enabled;
+    private String service;
 
     public boolean isEnabled() {
         return enabled;
@@ -25,7 +27,15 @@ public class JournalføringConfig {
         this.enabled = enabled;
     }
 
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getService() {
+        return Optional.ofNullable(service).orElse(SERVICE);
+    }
+
     public URI pingURI() {
-        return URIUtil.uri(SERVICE, DEFAULT_PING_PATH);
+        return URIUtil.uri(getService(), DEFAULT_PING_PATH);
     }
 }
