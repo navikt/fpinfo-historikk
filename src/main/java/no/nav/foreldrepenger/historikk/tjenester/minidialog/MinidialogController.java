35e5fed72aab3d6078488ac23a14f90c7efdbbda
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-24 16:58:28 +0200

    test token

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/minidialog/MinidialogController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/minidialog/MinidialogController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import no.nav.foreldrepenger.historikk.tjenester.sts.STStjeneste;
 import no.nav.security.oidc.api.ProtectedWithClaims;
 
 @RestController
@@ -18,23 +17,14 @@ public class MinidialogController {
 
     private static final Logger LOG = LoggerFactory.getLogger(MinidialogController.class);
     private final MinidialogTjeneste minidialog;
-    private final STStjeneste sts;
 
-    MinidialogController(MinidialogTjeneste minidialog, STStjeneste sts) {
+    MinidialogController(MinidialogTjeneste minidialog) {
         this.minidialog = minidialog;
-        this.sts = sts;
     }
 
     @GetMapping("/me")
     public List<MinidialogInnslag> hentMinidialog() {
-        try {
-            LOG.info("Henter token");
-            String token = sts.accessToken();
-            LOG.info("Fikk token", token);
-        } catch (Exception e) {
-            LOG.info("Fikk ikke token", e);
 
-        }
         return minidialog.hentMineAktiveDialoger();
     }
 
