ae9b3232dcc9f559c3bbb80b33eff50e43301173
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-25 11:10:07 +0200

    tema

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/journalføring/Journalpost.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/journalføring/Journalpost.java
@@ -7,22 +7,68 @@ import no.nav.foreldrepenger.historikk.util.Pair;
 public class Journalpost {
 
     private final JournalpostType journalpostType;
-    private final AvsenderMottaker avsenderMotaker;
+    private final AvsenderMottaker avsenderMottaker;
     private final Bruker bruker;
     private final BehandlingTema behandlingstema;
     private final String tittel;
     private final String journalførendeEnhet;
     private final String externReferanseId;
+    private final String tema;
     List<Pair<String, String>> tilleggsOpplysninger;
     private final Sak sak;
     private final List<Dokument> dokumenter;
+
+    public JournalpostType getJournalpostType() {
+        return journalpostType;
+    }
+
+    public AvsenderMottaker getAvsenderMottaker() {
+        return avsenderMottaker;
+    }
+
+    public Bruker getBruker() {
+        return bruker;
+    }
+
+    public BehandlingTema getBehandlingstema() {
+        return behandlingstema;
+    }
+
+    public String getTittel() {
+        return tittel;
+    }
+
+    public String getJournalførendeEnhet() {
+        return journalførendeEnhet;
+    }
+
+    public String getExternReferanseId() {
+        return externReferanseId;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public List<Pair<String, String>> getTilleggsOpplysninger() {
+        return tilleggsOpplysninger;
+    }
+
+    public Sak getSak() {
+        return sak;
+    }
+
+    public List<Dokument> getDokumenter() {
+        return dokumenter;
+    }
 
     public Journalpost(JournalpostType journalpostType, AvsenderMottaker avsenderMotaker, Bruker bruker,
             BehandlingTema behandlingstema, String tittel, String journalførendeEnhet, String externReferanseId,
             List<Pair<String, String>> tilleggsOpplysninger, Sak sak, List<Dokument> dokumenter) {
         this.journalpostType = journalpostType;
-        this.avsenderMotaker = avsenderMotaker;
+        this.avsenderMottaker = avsenderMotaker;
         this.bruker = bruker;
+        this.tema = "FOR";
         this.behandlingstema = behandlingstema;
         this.tittel = tittel;
         this.journalførendeEnhet = journalførendeEnhet;
@@ -35,7 +81,8 @@ public class Journalpost {
     @Override
     public String toString() {
         return getClass().getSimpleName() + "[journalportType=" + journalpostType + ", avsenderMotaker="
-                + avsenderMotaker + ", bruker=" + bruker + ", behandlingstema=" + behandlingstema + ", tittel=" + tittel
+                + avsenderMottaker + ", bruker=" + bruker + ", behandlingstema=" + behandlingstema + ", tittel="
+                + tittel
                 + ", journalførendeEnhet=" + journalførendeEnhet + ", externReferanseId=" + externReferanseId
                 + ", tilleggsOpplysninger=" + tilleggsOpplysninger + ", sak=" + sak + ", dokumenter=" + dokumenter
                 + "]";
