7fce6e60ced7e1f6b288307240ded04a4fa41dc2
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-28 09:09:44 +0200

    logging

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/journalføring/JournalføringTjeneste.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/journalføring/JournalføringTjeneste.java
@@ -2,6 +2,8 @@ package no.nav.foreldrepenger.historikk.tjenester.journalføring;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import io.github.resilience4j.retry.annotation.Retry;
@@ -10,6 +12,8 @@ import io.github.resilience4j.retry.annotation.Retry;
 @Retry(name = "dokarkiv")
 public class JournalføringTjeneste {
 
+    private static final Logger LOG = LoggerFactory.getLogger(JournalføringTjeneste.class);
+
     private final JournalføringConnection connection;
 
     public JournalføringTjeneste(JournalføringConnection connection) {
@@ -17,6 +21,7 @@ public class JournalføringTjeneste {
     }
 
     public String journalfør(Journalpost journalpost, boolean sluttfør) {
+        LOG.info("Oppretter journalpost {}", journalpost);
         return Optional.ofNullable(connection.opprettJournalpost(journalpost, sluttfør))
                 .map(JournalføringRespons::getJournalpostId)
                 .orElse(null);
