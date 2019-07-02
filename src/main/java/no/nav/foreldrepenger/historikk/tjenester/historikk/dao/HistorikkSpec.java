3ade0adfd57b40185025a0e8b04131d8bd975796
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-07-02 18:57:46 +0200

    cleanup

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/historikk/dao/HistorikkSpec.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/historikk/dao/HistorikkSpec.java
@@ -1,6 +1,5 @@
 package no.nav.foreldrepenger.historikk.tjenester.historikk.dao;
 
-import static no.nav.foreldrepenger.historikk.tjenester.historikk.dao.JPAHistorikkInnslag_.aktørId;
 import static no.nav.foreldrepenger.historikk.tjenester.historikk.dao.JPAHistorikkInnslag_.fnr;
 import static no.nav.foreldrepenger.historikk.tjenester.historikk.dao.JPAHistorikkInnslag_.opprettet;
 
@@ -8,17 +7,12 @@ import java.time.LocalDate;
 
 import org.springframework.data.jpa.domain.Specification;
 
-import no.nav.foreldrepenger.historikk.domain.AktørId;
 import no.nav.foreldrepenger.historikk.domain.Fødselsnummer;
 
 public final class HistorikkSpec {
 
     private HistorikkSpec() {
 
-    }
-
-    public static Specification<JPAHistorikkInnslag> harAktør(AktørId aktør) {
-        return (innslag, cq, cb) -> cb.equal(innslag.get(aktørId), aktør);
     }
 
     public static Specification<JPAHistorikkInnslag> harFnr(Fødselsnummer f) {
--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/minidialog/dao/MinidialogSpec.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/minidialog/dao/MinidialogSpec.java
@@ -2,16 +2,12 @@ package no.nav.foreldrepenger.historikk.tjenester.minidialog.dao;
 
 import static java.time.LocalDate.now;
 import static no.nav.foreldrepenger.historikk.tjenester.minidialog.dao.JPAMinidialogInnslag_.aktiv;
-import static no.nav.foreldrepenger.historikk.tjenester.minidialog.dao.JPAMinidialogInnslag_.aktørId;
 import static no.nav.foreldrepenger.historikk.tjenester.minidialog.dao.JPAMinidialogInnslag_.fnr;
 import static no.nav.foreldrepenger.historikk.tjenester.minidialog.dao.JPAMinidialogInnslag_.gyldigTil;
-import static no.nav.foreldrepenger.historikk.tjenester.minidialog.dao.JPAMinidialogInnslag_.hendelse;
 
 import org.springframework.data.jpa.domain.Specification;
 
-import no.nav.foreldrepenger.historikk.domain.AktørId;
 import no.nav.foreldrepenger.historikk.domain.Fødselsnummer;
-import no.nav.foreldrepenger.historikk.tjenester.innsending.Hendelse;
 
 public final class MinidialogSpec {
 
@@ -23,7 +19,7 @@ public final class MinidialogSpec {
         return (innslag, cq, cb) -> cb.greaterThanOrEqualTo(innslag.get(gyldigTil), now());
     }
 
-    public static Specification<JPAMinidialogInnslag> erGyldigTilNull() {
+    public static Specification<JPAMinidialogInnslag> gyldigErNull() {
         return (innslag, cq, cb) -> cb.isNull(innslag.get(gyldigTil));
     }
 
@@ -31,16 +27,7 @@ public final class MinidialogSpec {
         return (innslag, cq, cb) -> cb.isTrue(innslag.get(aktiv));
     }
 
-    public static Specification<JPAMinidialogInnslag> harAktør(AktørId a) {
-        return (innslag, cq, cb) -> cb.equal(innslag.get(aktørId), a);
-    }
-
     public static Specification<JPAMinidialogInnslag> harFnr(Fødselsnummer fødselsnummer) {
         return (innslag, cq, cb) -> cb.equal(innslag.get(fnr), fødselsnummer);
     }
-
-    public static Specification<JPAMinidialogInnslag> harHandling(Hendelse h) {
-        return (innslag, cq, cb) -> cb.equal(innslag.get(hendelse), h);
-    }
-
 }
