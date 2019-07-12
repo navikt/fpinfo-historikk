12ffa75161591dce3f9d9bc7f262498d86c4295a
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-07-12 22:10:20 +0200

    get cluster name

--- src/main/java/no/nav/foreldrepenger/historikk/config/ClusterAwareProfileSetter.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/ClusterAwareProfileSetter.java
@@ -1,10 +1,6 @@
 package no.nav.foreldrepenger.historikk.config;
 
 import static org.springframework.core.Ordered.LOWEST_PRECEDENCE;
-import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
@@ -13,7 +9,6 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
 import org.springframework.stereotype.Component;
 
 @Order(LOWEST_PRECEDENCE)
@@ -24,11 +19,8 @@ public class ClusterAwareProfileSetter implements ApplicationListener<Applicatio
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        LOG.info("XXXXXXXX");
-        Map<String, Object> prefixed = new LinkedHashMap<>();
-        prefixed.put("jalla", "42");
-        environment.getPropertySources()
-                .addAfter(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, new MapPropertySource("prefixer", prefixed));
+        String cluster = environment.getProperty("nais.cluster.name", "jalla");
+        LOG.info("XXXXXXXX " + cluster);
     }
 
     @Override
