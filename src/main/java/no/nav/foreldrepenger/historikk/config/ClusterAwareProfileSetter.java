dfa99748567bc9c60dcf05bc0bc3414b27b31cc9
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-07-12 21:54:16 +0200

    spring.factories

--- src/main/java/no/nav/foreldrepenger/historikk/config/ClusterAwareProfileSetter.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/ClusterAwareProfileSetter.java
@@ -1,5 +1,10 @@
 package no.nav.foreldrepenger.historikk.config;
 
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +12,7 @@ import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
 
 @Order(Ordered.LOWEST_PRECEDENCE)
 public class ClusterAwareProfileSetter implements EnvironmentPostProcessor {
@@ -16,6 +22,10 @@ public class ClusterAwareProfileSetter implements EnvironmentPostProcessor {
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         LOG.info("XXXXXXXX");
+        Map<String, Object> prefixed = new LinkedHashMap<>();
+        prefixed.put("jalla", "42");
+        environment.getPropertySources()
+                .addAfter(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, new MapPropertySource("prefixer", prefixed));
     }
 
 }
