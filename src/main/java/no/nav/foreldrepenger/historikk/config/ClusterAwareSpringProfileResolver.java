65ea6eedc9985cd07857345ccb8c4edc1304c9e9
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-08-15 19:53:50 +0200

    try again determining profile from cluster

--- src/main/java/no/nav/foreldrepenger/historikk/config/ClusterAwareSpringProfileResolver.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/ClusterAwareSpringProfileResolver.java
@@ -4,17 +4,12 @@ import static no.nav.foreldrepenger.historikk.util.EnvUtil.DEFAULT;
 import static no.nav.foreldrepenger.historikk.util.EnvUtil.DEV;
 import static no.nav.foreldrepenger.historikk.util.EnvUtil.LOCAL;
 
-import org.springframework.boot.logging.DeferredLog;
-
 public class ClusterAwareSpringProfileResolver {
 
     private static final String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
-    private static final DeferredLog LOG = new DeferredLog();
 
     public String getProfile() {
-        String cluster = clusterFra(System.getenv(NAIS_CLUSTER_NAME));
-        LOG.info("Vi er i cluster " + cluster);
-        return cluster;
+        return clusterFra(System.getenv(NAIS_CLUSTER_NAME));
     }
 
     private static String clusterFra(String cluster) {
@@ -24,8 +19,6 @@ public class ClusterAwareSpringProfileResolver {
         if (cluster.contains(DEV)) {
             return DEV;
         }
-
         return DEFAULT;
     }
-
 }
