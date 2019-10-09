5df30faa3599a07a61812b2b5e6285c354f20773
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-10-09 08:52:09 +0200

    fix EnvUtil

--- src/main/java/no/nav/foreldrepenger/historikk/config/SwaggerConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/SwaggerConfiguration.java
@@ -1,21 +1,19 @@
 package no.nav.foreldrepenger.historikk.config;
 
+import static com.google.common.collect.Sets.newHashSet;
 import static io.swagger.models.Scheme.HTTP;
 import static io.swagger.models.Scheme.HTTPS;
-import static java.util.stream.Collectors.toSet;
+import static no.nav.foreldrepenger.historikk.util.EnvUtil.isLocal;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 import java.util.Set;
-import java.util.stream.Stream;
 
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
-import io.swagger.models.Scheme;
-import no.nav.foreldrepenger.historikk.util.EnvUtil;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,22 +26,15 @@ public class SwaggerConfiguration implements EnvironmentAware {
     @Bean
     public Docket api() {
         return new Docket(SWAGGER_2)
-                .protocols(allProtocols())
+                .protocols(protocol())
                 .select()
                 .apis(basePackage("no.nav.foreldrepenger.historikk"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
-    private Set<String> allProtocols() {
-        if (EnvUtil.isLocal(env)) {
-            Stream.of(HTTP)
-                    .map(Scheme::toValue)
-                    .collect(toSet());
-        }
-        return Stream.of(HTTPS)
-                .map(Scheme::toValue)
-                .collect(toSet());
+    private Set<String> protocol() {
+        return isLocal(env) ? newHashSet(HTTP.toValue()) : newHashSet(HTTPS.toValue());
     }
 
     @Override
