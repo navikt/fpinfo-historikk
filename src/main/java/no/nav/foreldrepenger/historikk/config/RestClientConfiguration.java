5fab889d03cfe9df6a47ad3e60d22c2451fce52a
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-10-23 13:56:52 +0200

    try some recovery

--- src/main/java/no/nav/foreldrepenger/historikk/config/RestClientConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/RestClientConfiguration.java
@@ -3,6 +3,9 @@ package no.nav.foreldrepenger.historikk.config;
 import static no.nav.foreldrepenger.historikk.util.EnvUtil.LOCAL;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +21,10 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.RetryListener;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.client.RestOperations;
 
 import no.nav.foreldrepenger.historikk.http.MDCValuesPropagatingClienHttpRequesInterceptor;
@@ -106,6 +113,37 @@ public class RestClientConfiguration {
                 return execution.execute(request, body);
             }
         };
+    }
+
+    @Bean
+    public List<RetryListener> retryListeners() {
+        Logger log = LoggerFactory.getLogger(getClass());
+
+        return Collections.singletonList(new RetryListener() {
+
+            @Override
+            public <T, E extends Throwable> boolean open(RetryContext context, RetryCallback<T, E> callback) {
+                Field labelField = ReflectionUtils.findField(callback.getClass(), "val$label");
+                ReflectionUtils.makeAccessible(labelField);
+                String label = (String) ReflectionUtils.getField(labelField, callback);
+                log.trace("Starting retryable method {}", label);
+                return true;
+            }
+
+            @Override
+            public <T, E extends Throwable> void onError(RetryContext context, RetryCallback<T, E> callback,
+                    Throwable throwable) {
+                log.warn("Retryable method {} threw {}th exception {}",
+                        context.getAttribute("context.name"), context.getRetryCount(), throwable.toString());
+            }
+
+            @Override
+            public <T, E extends Throwable> void close(RetryContext context, RetryCallback<T, E> callback,
+                    Throwable throwable) {
+                log.trace("Finished retryable method {} {}", context.getRetryCount(),
+                        context.getAttribute("context.name"));
+            }
+        });
     }
 
 }
--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/oppslag/OppslagTjeneste.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/oppslag/OppslagTjeneste.java
@@ -1,13 +1,21 @@
 package no.nav.foreldrepenger.historikk.tjenester.oppslag;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
 
 import no.nav.foreldrepenger.historikk.domain.AktørId;
 import no.nav.foreldrepenger.historikk.domain.Fødselsnummer;
 
 @Service
 public class OppslagTjeneste implements Oppslag {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OppslagTjeneste.class);
 
     private final OppslagConnection connection;
 
@@ -25,14 +33,28 @@ public class OppslagTjeneste implements Oppslag {
     }
 
     @Override
+    @Retryable(value = { HttpServerErrorException.class }, maxAttempts = 3, backoff = @Backoff(delay = 1000))
     public String personNavn(Fødselsnummer fnr) {
         return connection.hentNavn(fnr);
     }
 
     @Cacheable(cacheNames = "organisasjon")
+    @Retryable(value = { HttpServerErrorException.class }, maxAttempts = 3, backoff = @Backoff(delay = 1000))
     @Override
     public String orgNavn(String orgnr) {
         return connection.orgNavn(orgnr);
+    }
+
+    @Recover
+    public String personNavnRecovery(HttpServerErrorException e, Fødselsnummer fnr) {
+        LOG.info("Retry failure recovery fnr");
+        return fnr.getFnr();
+    }
+
+    @Recover
+    public String orgNavnRecovery(HttpServerErrorException e, String orgnr) {
+        LOG.info("Retry failure recovery orgnr");
+        return orgnr;
     }
 
     @Override
