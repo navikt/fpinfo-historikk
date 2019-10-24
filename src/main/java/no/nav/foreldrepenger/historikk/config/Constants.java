41b7ffb356d666b5da40cefed4fbf3e1a6d4e5ca
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-10-24 12:54:53 +0200

    try adding callid

--- src/main/java/no/nav/foreldrepenger/historikk/config/Constants.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/Constants.java
@@ -1,10 +1,12 @@
 package no.nav.foreldrepenger.historikk.config;
 
 public final class Constants {
+    public static final String NAV_PERSONIDENTER = "Nav-Personidenter";
     public static final String SELVBETJENING = "selvbetjening";
     public static final String UKJENT_KODEVERKSVERDI = "-";
     public static final String NAV_CONSUMER_ID = "Nav-Consumer-Id";
     public static final String NAV_CALL_ID = "Nav-CallId";
+    public static final String NAV_CALL_ID1 = "Nav-Call-Id";
     public static final String CALL_ID = "callId";
     public static final String NAV_USER_ID = "Nav-User-Id";
     public static final String NAV_AKTØR_ID = "Nav-Aktør-Id";
