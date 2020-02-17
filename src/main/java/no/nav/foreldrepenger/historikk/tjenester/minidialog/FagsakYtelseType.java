f598d2fa1a660b225e6b10e8bfb53f36d221a480
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-02-17 13:26:10 +0100

    @JsonValue

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/minidialog/FagsakYtelseType.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/minidialog/FagsakYtelseType.java
@@ -1,11 +1,14 @@
 package no.nav.foreldrepenger.historikk.tjenester.minidialog;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum FagsakYtelseType {
     ENGANGSSTONAD("ES"),
     FORELDREPENGER("FP"),
     SVANGERSKAPSPENGER("SVP"),
     UDEFINERT("-");
 
+    @JsonValue
     private final String navn;
 
     private FagsakYtelseType(String navn) {
