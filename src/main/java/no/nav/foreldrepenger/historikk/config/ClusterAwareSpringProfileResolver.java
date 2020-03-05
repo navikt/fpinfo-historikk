6cfd805971e6d9450a1eefee698a08eeee198afe
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-03-05 11:03:25 +0100

    bumped kotrakter

--- src/main/java/no/nav/foreldrepenger/historikk/config/ClusterAwareSpringProfileResolver.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/ClusterAwareSpringProfileResolver.java
@@ -8,7 +8,12 @@ import static no.nav.foreldrepenger.boot.conditionals.EnvUtil.LOCAL;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public final class ClusterAwareSpringProfileResolver {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ClusterAwareSpringProfileResolver.class);
 
     private static final String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
 
@@ -24,6 +29,8 @@ public final class ClusterAwareSpringProfileResolver {
 
     private static String clusterFra(String cluster) {
         if (cluster == null) {
+            LOG.info("{} ikke satt, setter til {}", NAIS_CLUSTER_NAME, LOCAL);
+            System.setProperty(NAIS_CLUSTER_NAME, LOCAL);
             return LOCAL;
         }
         if (cluster.equals(DEV_FSS)) {
