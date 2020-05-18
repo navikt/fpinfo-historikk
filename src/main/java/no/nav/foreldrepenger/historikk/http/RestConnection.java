6535a3781ed4565621c5e7dda33bf7d54a8a8a2a
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-05-18 08:44:26 +0200

    RetryAware marker interface

--- src/main/java/no/nav/foreldrepenger/historikk/http/RestConnection.java
+++ src/main/java/no/nav/foreldrepenger/historikk/http/RestConnection.java
@@ -3,16 +3,6 @@ package no.nav.foreldrepenger.historikk.http;
 import java.net.URI;
 
 import org.springframework.http.HttpHeaders;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
-import org.springframework.web.client.HttpClientErrorException.Forbidden;
-import org.springframework.web.client.HttpServerErrorException.BadGateway;
-import org.springframework.web.client.HttpServerErrorException.InternalServerError;
-import org.springframework.web.client.ResourceAccessException;
-
-@Retryable(include = { ResourceAccessException.class,
-        BadGateway.class }, exclude = { InternalServerError.class,
-                Forbidden.class }, maxAttemptsExpression = "#{${rest.retry.attempts:3}}", backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
 
 public interface RestConnection {
 
--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/oppslag/Oppslag.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/oppslag/Oppslag.java
@@ -2,8 +2,9 @@ package no.nav.foreldrepenger.historikk.tjenester.oppslag;
 
 import no.nav.foreldrepenger.historikk.domain.AktørId;
 import no.nav.foreldrepenger.historikk.domain.Fødselsnummer;
+import no.nav.foreldrepenger.historikk.tjenester.felles.RetryAware;
 
-public interface Oppslag {
+public interface Oppslag extends RetryAware {
 
     AktørId aktørId();
 
