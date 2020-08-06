77155c35808075d2e0f879aa9795c569a106e8ac
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-08-06 11:19:23 +0200

    openAPI

--- src/main/java/no/nav/foreldrepenger/historikk/config/SwaggerConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/SwaggerConfiguration.java
@@ -2,40 +2,27 @@ package no.nav.foreldrepenger.historikk.config;
 
 import static io.swagger.models.Scheme.HTTP;
 import static io.swagger.models.Scheme.HTTPS;
-import static no.nav.foreldrepenger.boot.conditionals.EnvUtil.isLocal;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
 import java.util.Set;
 
-import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @Configuration
-@EnableSwagger2WebMvc
-public class SwaggerConfiguration implements EnvironmentAware {
-    private Environment env;
+@EnableOpenApi
+public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
         return new Docket(SWAGGER_2)
-                .protocols(protocol())
+                .protocols(Set.of(HTTP.toValue(), HTTPS.toValue()))
                 .select()
                 .apis(basePackage("no.nav.foreldrepenger.historikk"))
                 .build();
-    }
-
-    private Set<String> protocol() {
-        return isLocal(env) ? Set.of(HTTP.toValue()) : Set.of(HTTPS.toValue());
-    }
-
-    @Override
-    public void setEnvironment(Environment env) {
-        this.env = env;
     }
 }
