19b173fe2e4621ab39a56d3d6af00f976a18657f
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-09-01 13:29:41 +0200

    OAS30

--- src/main/java/no/nav/foreldrepenger/historikk/config/SwaggerConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/SwaggerConfiguration.java
@@ -3,7 +3,7 @@ package no.nav.foreldrepenger.historikk.config;
 import static io.swagger.models.Scheme.HTTP;
 import static io.swagger.models.Scheme.HTTPS;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
-import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+import static springfox.documentation.spi.DocumentationType.OAS_30;
 
 import java.util.Set;
 
@@ -19,7 +19,7 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
-        return new Docket(SWAGGER_2)
+        return new Docket(OAS_30)
                 .protocols(Set.of(HTTP.toValue(), HTTPS.toValue()))
                 .select()
                 .apis(basePackage("no.nav.foreldrepenger.historikk"))
