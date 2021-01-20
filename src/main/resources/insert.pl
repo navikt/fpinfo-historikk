f9f180118e1b469f0945404bf31dc5203134bfc8
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-01-20 14:10:55 +0100

    vault bump

--- src/main/resources/insert.pl
+++
@@ -1,2 +0,0 @@
-# takk til perl guru kjell.arne.rekaa@gmail.com for dette lille hacket 
-perl -F, -ane "\$F[0]=~s/^\s+//;print \"insert into dittnavoppgaver(fnr,referanseid) values('\$F[0]','\$F[1]');\n\"" historikk.dat
