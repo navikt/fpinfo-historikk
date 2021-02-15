72e6b418205e84be03260e12f50f825b5af986df
Jan-Olav Eide <janolaveide@gmail.com>
2021-02-15 11:44:34 +0100

    Revert "bump && post construct2"

---
+++ src/main/resources/insert.pl
@@ -0,0 +1,2 @@
+# takk til perl guru kjell.arne.rekaa@gmail.com for dette lille hacket 
+perl -F, -ane "\$F[0]=~s/^\s+//;print \"insert into dittnavoppgaver(fnr,referanseid) values('\$F[0]','\$F[1]');\n\"" historikk.dat
