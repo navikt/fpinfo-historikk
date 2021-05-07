2df62e5129dcfa8eb1ae7db1c10958a24eac5bf7
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-07 11:21:21 +0200

    annotate

--- src/main/java/no/nav/foreldrepenger/historikk/FPInfoHistorikkApplication.java
+++ src/main/java/no/nav/foreldrepenger/historikk/FPInfoHistorikkApplication.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.retry.annotation.EnableRetry;
 
+import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client;
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 
 @ConfigurationPropertiesScan("no.nav.foreldrepenger.historikk")
@@ -19,6 +20,7 @@ import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 @EnableCaching
 @EnableRetry
 @EnableJpaAuditing
+@EnableOAuth2Client(cacheEnabled = true)
 public class FPInfoHistorikkApplication {
     public static void main(String[] args) {
 
