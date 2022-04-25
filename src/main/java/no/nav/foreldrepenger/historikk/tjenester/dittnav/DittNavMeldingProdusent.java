f0ba790062a245cd619c5cd1bfe506f111359655
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-04-25 10:37:33 +0200

    Ta i bruk nye kolonner

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/dittnav/DittNavMeldingProdusent.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/dittnav/DittNavMeldingProdusent.java
@@ -12,7 +12,6 @@ import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
-import no.nav.foreldrepenger.historikk.domain.Fødselsnummer;
 import no.nav.foreldrepenger.historikk.tjenester.innsending.InnsendingHendelse;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
@@ -20,10 +19,6 @@ import org.springframework.util.concurrent.ListenableFutureCallback;
 public class DittNavMeldingProdusent implements DittNav {
 
     private static final Logger LOG = LoggerFactory.getLogger(DittNavMeldingProdusent.class);
-
-    private static final String LEGACY_OPPGAVE = "O";
-    private static final String OPPGAVE = "C";
-    private static final String BESKJED = "B";
 
     private final DittNavConfig config;
     private final DittNavMeldingsHistorikk lager;
@@ -38,65 +33,46 @@ public class DittNavMeldingProdusent implements DittNav {
     }
 
     @Override
-    public void avsluttOppgave(Fødselsnummer fnr, String grupperingsId, String eventId) {
-        // Litt slalåm i denne omgang..
-        // Hittil har vi lagret og sendt eventer som UUID med prefix B eller O. Etter overgang Aiven krever
-        // Brukernotifikasjon ren UUID uten prefix. Unntaket er ved Done-event til eksisterende beholdning.
-        var jpaOppgave = lager.hentOppgave(LEGACY_OPPGAVE + eventId, OPPGAVE + eventId);
-        if (jpaOppgave != null) {
-            var lokalReferanse = jpaOppgave.getReferanseId();
-            lager.slett(lokalReferanse);
-            var keyEventId = LEGACY_OPPGAVE.equals(lokalReferanse.substring(0,1))
-                ? lokalReferanse
-                : eventId;
-            var key = avsluttNøkkel(fnr, keyEventId, grupperingsId);
-            LOG.info("Avslutter oppgave med eventId: {}, grupperingsId: {}", key.getEventId(), grupperingsId);
-            send(avslutt(), key, config.getDone());
-        } else {
-            LOG.info("Ingen oppgave å avslutte i Ditt Nav");
-        }
+    public void avsluttOppgave(String eventId) {
+        lager.hentAktivOppgave(eventId)
+            .ifPresentOrElse(oppgave -> {
+                lager.avslutt(oppgave);
+                var key = avsluttNøkkel(oppgave.getFnr(), oppgave.getEksternReferanseId(), oppgave.getGrupperingsId());
+                LOG.info("Avslutter oppgave med eventId: {}, grupperingsId: {}", key.getEventId(), key.getGrupperingsId());
+                send(avslutt(), key, config.getDone());
+            }, () -> LOG.info("Ingen oppgave å avslutte i Ditt Nav"));
     }
 
     @Override
     public void opprettBeskjed(InnsendingHendelse h, String tekst) {
-        var internReferanse = BESKJED + h.getReferanseId();
-        if (!lager.erOpprettet(internReferanse)) {
-            NokkelInput key;
-            var eventId = UUID.randomUUID().toString();
-            if (h.getSaksnummer() != null) {
-                key = nøkkel(h.getFnr(), eventId, h.getSaksnummer());
-                LOG.info("Oppretter beskjed med rnd eventId: {}, internReferanse: {}, saksnummer: {}, tekst: {}",
-                    key.getEventId(), internReferanse, h.getSaksnummer(), tekst);
-            } else {
-                key = nøkkel(h.getFnr(), eventId, UUID.randomUUID().toString());
-                LOG.info("Oppretter beskjed med random eventId: {}, random grupperingsId {} (mangler saksnummer), internReferanse: {}, tekst: {}",
-                    key.getEventId(), key.getGrupperingsId(), internReferanse, tekst);
-            }
+        if (!lager.erOpprettetBeskjed(h.getReferanseId())) {
+            var key = nøkkel(h.getFnr(), UUID.randomUUID().toString(), grupperingsId(h));
+            LOG.info("Oppretter beskjed med eventId {}, internReferanseId {}, grupperingId {}, tekst {}",
+                key.getEventId(), h.getReferanseId(), key.getGrupperingsId(), tekst);
             send(beskjed(tekst, config.uri(), config.getBeskjedVarighet()), key, config.getBeskjed());
-            lager.opprettBeskjed(h.getFnr(), key.getGrupperingsId(), h.getReferanseId(), eventId);
+            lager.opprettBeskjed(h.getFnr(), key.getGrupperingsId(), h.getReferanseId(), key.getEventId());
         } else {
-            LOG.info("Det er allerede opprettet en beskjed {} ", internReferanse);
+            LOG.info("Det er allerede opprettet en beskjed {} ", h.getReferanseId());
         }
     }
 
     @Override
     public void opprettOppgave(InnsendingHendelse h, String tekst) {
-        if (!lager.erOpprettet(LEGACY_OPPGAVE + h.getReferanseId())
-            && !lager.erOpprettet(OPPGAVE + h.getReferanseId())) {
-            var key = nøkkel(h.getFnr(), h.getReferanseId(), h.getSaksnummer());
+        if (!lager.erOpprettetOppgave(h.getReferanseId())) {
+            var key = nøkkel(h.getFnr(), UUID.randomUUID().toString(), h.getSaksnummer());
             LOG.info("Oppretter oppgave med eventId: {}, saksnummer: {}, tekst: {} i Ditt Nav",
                 key.getEventId(), h.getSaksnummer(), tekst);
             send(oppgave(tekst, config.uri(), config.getOppgaveVarighet()), key, config.getOppgave());
-            lager.opprettOppgave(h.getFnr(), key.getGrupperingsId(), h.getReferanseId(), h.getReferanseId());
+            lager.opprettOppgave(h.getFnr(), key.getGrupperingsId(), h.getReferanseId(), key.getEventId());
         } else {
             LOG.info("Det er allerede opprettet en oppgave for referanseId {} ", h.getReferanseId());
         }
     }
 
     private void send(Object msg, NokkelInput key, String topic) {
-        var melding = new ProducerRecord<NokkelInput, Object>(topic, key, msg);
+        var melding = new ProducerRecord<>(topic, key, msg);
         LOG.info("Sender melding med id {} på {}", key.getEventId(), topic);
-        kafkaOperations.send(melding).addCallback(new ListenableFutureCallback<SendResult<NokkelInput, Object>>() {
+        kafkaOperations.send(melding).addCallback(new ListenableFutureCallback<>() {
 
             @Override
             public void onSuccess(SendResult<NokkelInput, Object> result) {
@@ -109,6 +85,14 @@ public class DittNavMeldingProdusent implements DittNav {
                 LOG.warn("Kunne ikke sende melding {} med id {} på {}", msg, key.getEventId(), topic, e);
             }
         });
+    }
+
+    private String grupperingsId(InnsendingHendelse h) {
+        if (h.getSaksnummer() == null) {
+            LOG.info("Hendelse har ikke tilknyttet saksnummer, benytter random grupperingId");
+            return UUID.randomUUID().toString();
+        }
+        return h.getSaksnummer();
     }
 
     @Override
