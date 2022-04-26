d57bbe5a074e11cf3277c819504fbf9d3ad11cc0
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-04-26 16:24:34 +0200

    Hent oppgaver

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/dittnav/DittNavAdminController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/dittnav/DittNavAdminController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import static no.nav.foreldrepenger.historikk.tjenester.dittnav.DittNavAdminController.ADMIN;
 
 
@@ -20,11 +21,20 @@ public class DittNavAdminController {
     public static final String ADMIN = "/internal/admin";
     private static final Logger LOG = LoggerFactory.getLogger(DittNavAdminController.class);
     private static final Logger SECURE_LOG = LoggerFactory.getLogger("secureLogger");
+    final DittNavMeldingsHistorikk lager;
 
+    final JPADittNavOppgaverRepository dao;
+
+    public DittNavAdminController(DittNavMeldingsHistorikk lager, JPADittNavOppgaverRepository dao) {
+        this.lager = lager;
+        this.dao = dao;
+    }
 
     @GetMapping
     public String verifiser() {
-        SECURE_LOG.info("Test til secure log");
+        var deployDato = LocalDateTime.of(2022, 3, 25, 11, 27);
+        var førsteOppgaver = dao.ikkeAvsluttedeOppgaver(deployDato, 5);
+        SECURE_LOG.info("Test til secure log. Første internReferanseIds vedlagt", førsteOppgaver);
         return "Hei autentisert bruker";
     }
 
--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/dittnav/JPADittNavOppgaverRepository.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/dittnav/JPADittNavOppgaverRepository.java
@@ -5,7 +5,12 @@ import static no.nav.foreldrepenger.historikk.config.JpaTxConfiguration.JPA_TM;
 import no.nav.foreldrepenger.historikk.tjenester.dittnav.JPADittNavOppgave.NotifikasjonType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Transactional(JPA_TM)
 public interface JPADittNavOppgaverRepository
@@ -14,5 +19,15 @@ public interface JPADittNavOppgaverRepository
     JPADittNavOppgave findByInternReferanseIdIgnoreCase(String referanseId);
 
     boolean existsByInternReferanseIdIgnoreCaseAndType(String referanseId, NotifikasjonType type);
+
+    @Query(value = """
+            select o.ekstern_referanse_id from dittnavoppgaver o
+            where o.ekstern_referanse_id is not null
+            and o.sendt_done = false
+            and o.opprettet < :terskel
+            and o.type = "OPPGAVE"
+            limit :noOfRows
+            """, nativeQuery = true)
+    List<String> ikkeAvsluttedeOppgaver(@Param("terskel") LocalDateTime terskel, @Param("noOfRows") int noOfRows);
 
 }
