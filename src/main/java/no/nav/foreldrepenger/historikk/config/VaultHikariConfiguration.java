be919040a03825d1db8c46024ca9669317c8de1e
Espen Waaga <espen.waaga@nav.no>
2022-06-03 10:31:02 +0200

    Rydder i VaultHikariConfiguration

--- src/main/java/no/nav/foreldrepenger/historikk/config/VaultHikariConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/VaultHikariConfiguration.java
@@ -2,7 +2,7 @@ package no.nav.foreldrepenger.historikk.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
@@ -10,25 +10,23 @@ import org.springframework.context.annotation.Configuration;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
-@ConditionalOnProperty(value = "spring.cloud.vault.enabled")
-public class VaultHikariConfiguration implements InitializingBean {
+@ConditionalOnProperty(value = "spring.cloud.vault.database.enabled")
+public class VaultHikariConfiguration {
     private static final Logger LOG = LoggerFactory.getLogger(VaultHikariConfiguration.class);
+
     private final HikariDataSource ds;
 
-    @Value("${spring.datasource.username}")
-    private String username;
-    @Value("${spring.datasource.password}")
-    private String password;
-
-    public VaultHikariConfiguration(HikariDataSource ds) {
+    @Autowired
+    public VaultHikariConfiguration(@Value("${spring.datasource.username}") String username,
+                                    @Value("${spring.datasource.password}") String password,
+                                    HikariDataSource ds) {
         this.ds = ds;
+        setUsernameAndPasswordForDS(username, password);
     }
 
-    @Override
-    public void afterPropertiesSet() {
-        LOG.info("Brukernavnet er {},", username);
-        if (password == null) {
-            LOG.warn("Passord er ikke satt.");
+    private void setUsernameAndPasswordForDS(String username, String password) {
+        if (username == null || password == null) {
+            LOG.warn("Brukernavn og/eller passord er null for datasource. Noe galt med Vault?");
         }
         ds.setUsername(username);
         ds.setPassword(password);
