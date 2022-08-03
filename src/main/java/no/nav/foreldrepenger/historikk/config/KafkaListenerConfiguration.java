c676a05cfc8370b69c3aec6a1a4d06027c879d2e
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-08-03 10:45:56 +0200

    Consumer retry med backoff uten commit. Logg melding til securelog. AuthenticationException stopper container.

--- src/main/java/no/nav/foreldrepenger/historikk/config/KafkaListenerConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/historikk/config/KafkaListenerConfiguration.java
@@ -1,6 +1,8 @@
 package no.nav.foreldrepenger.historikk.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,15 +11,25 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistrar;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.CommonContainerStoppingErrorHandler;
+import org.springframework.kafka.listener.CommonDelegatingErrorHandler;
+import org.springframework.kafka.listener.CommonErrorHandler;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.util.backoff.FixedBackOff;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
+import javax.naming.AuthenticationException;
 import java.time.Duration;
+
+import static org.springframework.kafka.listener.ContainerProperties.AckMode.MANUAL_IMMEDIATE;
 
 @Configuration
 public class KafkaListenerConfiguration implements KafkaListenerConfigurer {
 
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaListenerConfiguration.class);
+    private static final Logger SECURE_LOG = LoggerFactory.getLogger("secureLogger");
     @Autowired
     private LocalValidatorFactoryBean validator;
 
@@ -33,7 +45,20 @@ public class KafkaListenerConfiguration implements KafkaListenerConfigurer {
         factory.setConsumerFactory(cf);
         factory.setMessageConverter(new StringJsonMessageConverter(mapper));
         factory.getContainerProperties().setAuthExceptionRetryInterval(Duration.ofSeconds(5L));
+        factory.getContainerProperties().setAckMode(MANUAL_IMMEDIATE);
+        factory.setCommonErrorHandler(errorHandler());
         return factory;
+    }
+
+    private static CommonErrorHandler errorHandler() {
+        var defaultHandler = new DefaultErrorHandler((record, exception) -> {
+            LOG.warn("Konsum av melding fra topic {} feilet, se secure log for detaljer.", record.topic(), exception);
+            SECURE_LOG.warn("Konsum av melding fra topic {} feilet. Melding: {}", record.topic(), record);
+        }, new FixedBackOff(1000L, 9L));
+        var delegatingHandler = new CommonDelegatingErrorHandler(defaultHandler);
+        delegatingHandler.addDelegate(AuthenticationException.class, new CommonContainerStoppingErrorHandler());
+        delegatingHandler.setAckAfterHandle(false);
+        return delegatingHandler;
     }
 
 }
