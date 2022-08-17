7fe6c994b8e6fbf0d99382a769e049badf0d6e73
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-08-17 14:12:59 +0200

    Revert "Logger topic+offset"
    This reverts commit 8444a7df3e5813b1f8c046da52c6c956f0147762.

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/inntektsmelding/InntektsmeldingHendelseKonsument.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/inntektsmelding/InntektsmeldingHendelseKonsument.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -30,11 +29,8 @@ public class InntektsmeldingHendelseKonsument {
     @Transactional
     @KafkaListener(topics = "#{'${historikk.inntektsmelding.topic}'}", groupId = "#{'${historikk.inntektsmelding.group-id}'}")
     public void konsumer(@Payload @Valid InntektsmeldingHendelse h,
-                         @Header(name = NAV_CALL_ID, required = false) String callId,
-                         @Header(KafkaHeaders.OFFSET) long offset,
-                         @Header(KafkaHeaders.TOPIC) String topic) {
+            @Header(name = NAV_CALL_ID, required = false) String callId) {
         toMDC(NAV_CALL_ID, callId);
-        LOG.info("Kafkamelding {} med offset {}", topic, offset);
         LOG.info("Mottok inntektsmeldinghendelse {}", h);
         inntektsmelding.lagre(h);
         LOG.info("Inntektsmeldinghendelse med referanse {} er lagret", h.getReferanseId());
