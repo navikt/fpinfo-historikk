cd2eac3e93c468bfd757b77e0d29d4cad7727d36
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-10-19 11:34:11 +0200

    Saf: sett media type

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/dokumentarkiv/ArkivController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/dokumentarkiv/ArkivController.java
@@ -2,8 +2,10 @@ package no.nav.foreldrepenger.historikk.tjenester.dokumentarkiv;
 
 import no.nav.boot.conditionals.ConditionalOnNotProd;
 import no.nav.foreldrepenger.historikk.http.ProtectedRestController;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ProtectedRestController(ArkivController.Arkiv)
 @ConditionalOnNotProd
@@ -16,8 +18,10 @@ public class ArkivController {
         this.arkiv = arkiv;
     }
 
-    @GetMapping("/hent-dokument")
-    public byte[] hent(@RequestParam(name = "dokumentId") String dokumentId) {
+    @GetMapping(
+        value ="/hent-dokument",
+        produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody byte[] hent(@RequestParam(name = "dokumentId") String dokumentId) {
         return arkiv.hentDokument(dokumentId);
     }
 
