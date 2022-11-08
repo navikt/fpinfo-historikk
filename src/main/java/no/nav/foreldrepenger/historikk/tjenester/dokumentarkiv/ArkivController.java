61599111c27aa85b10da4b1b99d81547162702e2
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-11-08 13:03:54 +0100

    Joarktjeneste: dokumentId som pathvariable

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/dokumentarkiv/ArkivController.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/dokumentarkiv/ArkivController.java
@@ -1,23 +1,16 @@
 package no.nav.foreldrepenger.historikk.tjenester.dokumentarkiv;
 
 import no.nav.boot.conditionals.ConditionalOnNotProd;
-import no.nav.security.token.support.spring.ProtectedRestController;
-import org.springframework.http.HttpStatus;
+import no.nav.foreldrepenger.historikk.http.ProtectedRestController;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
-import static no.nav.foreldrepenger.historikk.tjenester.dokumentarkiv.ArkivController.Arkiv;
-
-//@ProtectedRestController(ArkivController.Arkiv)
-@ProtectedRestController(value={ Arkiv }, issuer="tokenx")
-@ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
 @ConditionalOnNotProd
+@ProtectedRestController(ArkivController.Arkiv)
 public class ArkivController {
     public static final String Arkiv = "/arkiv";
 
@@ -30,7 +23,7 @@ public class ArkivController {
     @GetMapping(
         value ="/hent-dokument/{journalpostId}/{dokumentId}",
         produces = MediaType.APPLICATION_PDF_VALUE)
-    public @ResponseBody byte[] hent(@PathVariable(name = "journalpostId") String journalpostId, @RequestParam(name = "dokumentId") String dokumentId) {
+    public @ResponseBody byte[] hent(@PathVariable(name = "journalpostId") String journalpostId, @PathVariable(name = "dokumentId") String dokumentId) {
         return arkiv.hentPdf(journalpostId, dokumentId);
     }
 
