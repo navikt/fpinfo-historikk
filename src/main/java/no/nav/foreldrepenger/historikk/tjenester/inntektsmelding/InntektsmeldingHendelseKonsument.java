d6aca0103cd92c3fd4d434c608e2e199913d6741
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-12-16 13:23:26 +0100

    Legger til inntektsmelding topic

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/inntektsmelding/InntektsmeldingHendelseKonsument.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/inntektsmelding/InntektsmeldingHendelseKonsument.java
@@ -2,14 +2,15 @@ package no.nav.foreldrepenger.historikk.tjenester.inntektsmelding;
 
 import static no.nav.foreldrepenger.common.util.Constants.NAV_CALL_ID;
 import static no.nav.foreldrepenger.common.util.MDCUtil.toMDC;
+import static no.nav.foreldrepenger.historikk.config.KafkaListenerConfiguration.AIVEN;
 import static no.nav.foreldrepenger.historikk.config.KafkaOnpremListenerConfiguration.CFONPREM;
 
 import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
@@ -37,6 +38,22 @@ public class InntektsmeldingHendelseKonsument {
         LOG.info("Mottok inntektsmeldinghendelse {}", h);
         inntektsmelding.lagre(h);
         LOG.info("Inntektsmeldinghendelse med referanse {} er lagret", h.getReferanseId());
+    }
+
+    @Transactional
+    @KafkaListener(topics = "#{'${historikk.inntektsmelding-aiven.topic}'}",
+                   groupId = "#{'${historikk.inntektsmelding-aiven.group-id}'}",
+                   containerFactory = AIVEN)
+    public void aivenKonsumer(@Payload @Valid InntektsmeldingHendelse h,
+                              @Header(name = NAV_CALL_ID, required = false) String callId,
+                              @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                              @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partitionId,
+                              @Header(KafkaHeaders.OFFSET) int offset) {
+        toMDC(NAV_CALL_ID, callId);
+        LOG.info("Mottok Inntektsmeldinghendelse med referanseId {} fra {}, partition {}, offset {}",
+            h.getReferanseId(), topic, partitionId, offset);
+        inntektsmelding.lagre(h);
+        LOG.info("Lagret Inntektsmeldinghendelse med referanse {}", h.getReferanseId());
     }
 
     @Override
