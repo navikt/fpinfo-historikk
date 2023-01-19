d17b45267a11ed58a03a459232df287a3368b41f
Steffen Pøhner <steffen.h.pohner@nav.no>
2023-01-19 15:19:09 +0100

    Ikke initier webclient bønne for saf i prod

--- src/main/java/no/nav/foreldrepenger/historikk/tjenester/dokumentarkiv/SafWebclient.java
+++ src/main/java/no/nav/foreldrepenger/historikk/tjenester/dokumentarkiv/SafWebclient.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.historikk.tjenester.dokumentarkiv;
 
+import no.nav.boot.conditionals.ConditionalOnNotProd;
 import no.nav.foreldrepenger.historikk.http.TokenXExchangeFilterFunction;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import static no.nav.foreldrepenger.historikk.tjenester.dokumentarkiv.ArkivConnection.SAF;
 
 @Configuration
+@ConditionalOnNotProd
 public class SafWebclient {
 
     @Qualifier(SAF)
